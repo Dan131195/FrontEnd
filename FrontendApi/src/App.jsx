d8922fd9574e98ecdd526cf499f029bc9bd80ab4
Dan131195 <daniele.franceschini1395@gmail.com>
2025-04-02 11:52:38 +0200

    Login

--- FrontendApi/src/App.jsx
+++ FrontendApi/src/App.jsx
@@ -6,6 +6,8 @@ import VisitaForm from "./Components/VisitaForm";
 import VenditaForm from "./Components/VenditaForm";
 import VenditaList from "./Components/VenditaList";
 import RicoveriList from "./Components/RicoveriList";
+import HomePage from "./Components/HomePage";
+import PrivateRoute from "./Components/PrivateRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -13,9 +15,11 @@ const App = () => {
   return (
     <Router>
       <NavbarComponent />
+      {/* <PrivateRoute> */}
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<AnimaleList />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/animali/list" element={<AnimaleList />} />
           <Route path="/animali/nuovo" element={<AnimaleForm />} />
           <Route path="/visite/nuova" element={<VisitaForm />} />
           <Route path="/vendite/nuova" element={<VenditaForm />} />
@@ -23,6 +27,7 @@ const App = () => {
           <Route path="/ricoveri" element={<RicoveriList />} />
         </Routes>
       </div>
+      {/* </PrivateRoute> */}
     </Router>
   );
 };
